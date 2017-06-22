8b2612af71a17ec9980b883a564027dcf39e987d
Azzinoth <kandrat2007@yandex.ru>
2017-06-22 08:44:22 +0300

    first iteration

---
+++ main.js
@@ -0,0 +1,17 @@
+var InitWebGL = function() {
+    console.log('TEST!');
+
+    var canvas = document.getElementById('mainCanvas');
+    var gl = canvas.getContext('webgl');
+
+    if (!gl) {
+        gl = canvas.getContext('experimental-webgl');
+    }
+
+    if (!gl) {
+        alert('Your browser does not support WebGL');
+    }
+    
+    gl.clearColor(0.75, 0.85, 0.8, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+}
